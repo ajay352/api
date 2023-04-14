7b1df58e42147e1bbbf75e12b917b4b3bf7d520a
ajay352 <saharajay30@gmail.com>
2023-04-14 11:44:09 +0530

    tamil new year

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import pymysql
-from app import app
-from config import mysql
-from flask import jsonify
-from flask import flash,request
-import mysql.connector
 
-@app.route('/my',methods=['POST','GET'])
+from app import app
+from flask import jsonify
+from flask import request
+import mysql.connector
+@app.route('/create',methods=['POST','GET'])
 def create_emp():
     name=request.json['name']
     email=request.json['email']
@@ -23,10 +21,9 @@ def create_emp():
         return response
     else:
         return jsonify('error')
-
     
     
-@app.route('/emp')
+@app.route('/read')
 def emp():
     try:
         mydb=mysql.connector.connect(host='localhost',user='root',password='',database='ajay')
@@ -43,6 +40,16 @@ def emp():
         #cursor.close() 
         #conn.close()  
         pass
+@app.route('/read/<int:id>')
+def reax_one(id):
+        mydb=mysql.connector.connect(host='localhost',user='root',password='',database='ajay')
+        mycursor=mydb.cursor()
+        SQL="SELECT id, name, email, phonenumber, password FROM myregister WHERE id=%s"
+        mycursor.execute(SQL,(id,))
+        empRows = mycursor.fetchone()
+        respone = jsonify(empRows)
+        respone.status_code = 200
+        return respone
 @app.route('/delete/<int:id>',methods=['DELETE'])
 def delete(id):
     mydb=mysql.connector.connect(host='localhost',user='root',password='',database='ajay')
@@ -50,38 +57,34 @@ def delete(id):
     sql="DELETE FROM myregister WHERE id =%s"
     
     mycursor.execute(sql,(id,))
+    mydb.commit()
+ 
     
     respone = jsonify('Employee deleted successfully!')
     respone.status_code = 200
     return respone
-
-
-@app.route('/update/<int:id>',methods=['PUT'])
+@app.route('/update/<int:id>',methods=['POST','GET'])
 def update(id):
-    my=int(id,)
-    ids=request.json['id']
+    
+    
     name=request.json['name']
     email=request.json['email']
     phonenumber=request.json['phonenumber']
     password=request.json['password']
-
-    mydb=mysql.connector.connect(host='localhost',user='root',password='',database='ajay')
-    mycursor=mydb.cursor()
-    sql="UPDATE myregister SET name =%s,email=%s,phonenumber=%s,password=%s WHERE id =%s"
-    val=(name,email,phonenumber,password,ids)
-    mycursor.execute(sql,val)
-    response=jsonify('updated')
-    #response=jsonify(my)
-
-    response.status_code=200
-    return response
-
-
-
-
-
-
-
+    if name and email and phonenumber and password and request.method == 'POST':
+      mydb=mysql.connector.connect(host='localhost',user='root',password='',database='ajay')
+      mycursor=mydb.cursor()
+      sql="UPDATE myregister SET name =%s,email=%s,phonenumber=%s,password=%s WHERE id =%s"
+      val=(name,email,phonenumber,password,id)
+      mycursor.execute(sql,val)
+      mydb.commit()
+      response=jsonify('updated')
+    
+     
+      response.status_code=200
+      return response
+    else:
+        return jsonify('error')
 if __name__ =="__main__":
     
     app.run(debug=True)
